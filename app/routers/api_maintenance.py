5b082774a7588ba35ed5fe19ae21b4cb3848968c
Riley Beck <beckriley515@gmail.com>
2026-03-13 17:27:31 +0000

    added to the create maintenance item route.

--- app/routers/api_maintenance.py
+++ app/routers/api_maintenance.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Form
+from datetime import date
 from app.services.maintenance_storage import (
     load_maintenance_items,
     load_single_maintenance_item,
-    add_maintenance_item
+    add_maintenance_item,
 )
 
 
@@ -23,5 +24,12 @@ def get_single_item(id: str):
 
     return {"single_item": item_dic}
 
+
 @router.post("/api/maintenance/add-item")
-def add_item(name: str, ) #Keep adding to this it isn't done.
+def add_item(
+    title: str, info: str, due_date: str, equip: str
+):  # I need to to use form here. Also date is a string in this but the class expects a date object, so I need to convert it.
+
+    new_item = add_maintenance_item(title, info, due_date, equip)
+
+    return {"new_item": new_item.get_dict()}
