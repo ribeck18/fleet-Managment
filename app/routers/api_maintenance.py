9f95830878443c988e017ca5c6d7fbb5687ed5db
ElhokarsWit <beckriley515@gmail.com>
2026-03-13 10:56:02 -0600

    Refactor API maintenance routes to include loading single maintenance items and adding maintenance items

--- app/routers/api_maintenance.py
+++ app/routers/api_maintenance.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter, Form
-from app.services.maintenance_storage import load_maintenance_items
+from app.services.maintenance_storage import (
+    load_maintenance_items,
+    load_single_maintenance_item,
+    add_maintenance_item
+)
 
 
 router = APIRouter()
@@ -8,4 +12,16 @@ router = APIRouter()
 @router.get("/api/maintenance/load")
 def get_all():
     m_list = load_maintenance_items()
+
     return {"maintenance_list": m_list}
+
+
+@router.get("/api/maintenance/{id}/load")
+def get_single_item(id: str):
+    item_obj = load_single_maintenance_item(id)
+    item_dic = item_obj.get_dict()
+
+    return {"single_item": item_dic}
+
+@router.post("/api/maintenance/add-item")
+def add_item(name: str, ) #Keep adding to this it isn't done.
